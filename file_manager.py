ca24bc9e07058d49073b2c5de281fafdbc97ed4e
Julian <julianstrah@gmail.com>
2025-05-19 21:22:38 -0300

    use k8s volume

--- file_manager.py
+++ file_manager.py
@@ -1,17 +1,26 @@
+import os
 from logger import get_logger
 
-
-LAST_PROGRAM_FILE = "last_program.txt"
-
+LAST_PROGRAM_FILE = "/mnt/k3s-volumes/new_bb_programs_notification/last_program.txt"
 logger = get_logger(__name__)
 
 def get_saved_last_program():
     logger.debug("Getting saved last program")
-    with open(LAST_PROGRAM_FILE, "r") as f:
-        last_program = f.read()
-    return last_program
+    try:
+        if not os.path.exists(LAST_PROGRAM_FILE):
+            logger.info("Last program file does not exist. Returning empty string.")
+            return ""
+        with open(LAST_PROGRAM_FILE, "r") as f:
+            return f.read()
+    except Exception as e:
+        logger.error(f"Failed to read last program: {e}")
+        return ""
 
 def save_last_program(program):
     logger.debug("Saving last program")
-    with open(LAST_PROGRAM_FILE, "w") as f:
-        f.write(program)
+    try:
+        os.makedirs(os.path.dirname(LAST_PROGRAM_FILE), exist_ok=True)
+        with open(LAST_PROGRAM_FILE, "w") as f:
+            f.write(program)
+    except Exception as e:
+        logger.error(f"Failed to save last program: {e}")
